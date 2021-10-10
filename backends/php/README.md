8c99975ba6933cd3fce166e632cd8b0e06ca5736
Qchmqs <achmas@live.fr>
2021-10-10 12:26:59 +0100

    added php

---
+++ backends/php/README.md
@@ -0,0 +1,11 @@
+# Install PHP
+https://www.php.net/
+
+# install composer
+https://getcomposer.org/
+
+# install dependencies
+composer install
+
+# start the server
+composer start
---
+++ backends/php/public/index.php
@@ -0,0 +1,13 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello', function (Request $request, Response $response, array $args) {
+    $response = $response->withJson(["message" => "hello FROM THE OTHER SIDE"]);
+
+    return $response->withStatus((200))->withHeader('Access-Control-Allow-Origin', '*');
+});
+$app->run();
