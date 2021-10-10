8953374989b13d22653bb561bc2da0bc4f830b4c
Qchmqs <achmas@live.fr>
2021-10-10 14:12:04 +0100

    added a simple form post

--- backends/python/index.py
+++ backends/python/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,15 @@ def hello():
 
     return (response, 200)
 
+@app.route('/hi', methods = ['POST'])
+def hi():
+    hi = request.form.get('hi')
+    print(hi)
+    response = make_response({
+        'message': 'hi FROM THE OTHER {0}'.format(hi)
+    })
+
+    return (response, 200)
 
 if __name__ == '__main__':
     app.run(port=3000)
